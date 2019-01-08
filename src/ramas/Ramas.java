b3a99661e556e4a09162aaaa09321e23d912237a
dam <dam@172.26.219.117>
2019-01-08 09:26:39 +0100

    version 2.0

--- src/ramas/Ramas.java
+++ src/ramas/Ramas.java
@@ -16,7 +16,7 @@ public class Ramas {
      */
     public static void main(String[] args) {
       
-        System.out.println("version 1.0");
+        System.out.println("version 2.0");
     }
     
 }
